620bf3f73c393b549b47cc1529c57d36473bd1e1
Drew Pentland <drewtpentland@gmail.com>
2022-09-26 19:26:43 +0200

    Added add_to_vimwiki.sh script which can be used to convert a jupyter notebook to markdown and add it to my vimwiki. Check README for pre-made script.

--- README.md
+++ README.md
@@ -6,3 +6,9 @@
 - I started following along after video 10 where we started with NumPY.
 - Many of the notes I add here will be in the Jupyter notebook. It's fairly easy to make notes here and show a lesson.
 - TODO: Make a parser that runs through a jupyter notebook and converts the code and comments to markdown for vimwiki.
+
+
+- To copy over to vimwiki easily copy over notes:
+```sh
+./add_to_vimwiki.sh -f video_11.ipynb -d ~/Documents/vimwiki/Python3/ -i "Machine Learnia YouTube Course.md"
+```
--- add_to_vimwiki.sh
+++ add_to_vimwiki.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
+# ./add_to_vimwiki.sh -f [.ipynb file] -d [destination folder] -i [name of index.md]
+# This script will take a .ipynb file and convert it to markdown. Then move it into
+# the folder you specify.
+# Destination folder is optional. Default is current working directory.
+# Name of index is the name of the .md file that acts as the index for the folder.
 
 dest_path=".";
+index="";
 
-while getopts d:f: flag
+while getopts d:f:i: flag
 do
     case "${flag}" in
         f) filename=${OPTARG};;
         d) dest_path=${OPTARG};;
+        i) index=${OPTARG};;
     esac
 done
 
 echo "Filename: $filename";
-# jupyter nbconvert --to markdown $filename;
+jupyter nbconvert --to markdown $filename;
 filename="${filename%.*}"
 echo "Filename: $filename";
-mv ${filename}.md dest_path;
+mv ${filename}.md $dest_path;
+echo "[${filename}](${filename})" >> $dest_path/$index
